6618ccd667610ea02c893bc6b4d6ef9d782c2fb0
masOTROS <javier@masotros.com>
2013-06-26 11:01:46 -0300

    Corrected the class template overloading for ofImage.

--- src/ofxAnimatableObject.h
+++ src/ofxAnimatableObject.h
@@ -22,10 +22,16 @@ public:
         rotation.setPosition(ofPoint(0.,0.,0.));
     }
     
-    virtual ~ofxAnimatableObject(void){};
+    ~ofxAnimatableObject(){};
     
-    void update(float dt); //two cases for this function declared below
-    
+    void update(float dt){
+        size.update(dt);
+        color.update(dt);
+        position.update(dt);
+        rotation.update(dt);
+        T::update();
+    }
+
     void draw(){
         ofPushMatrix();
         ofPushStyle();
@@ -65,19 +71,60 @@ public:
 
 };
 
-template <class T>
-void ofxAnimatableObject<T>::update(float dt){
-    size.update(dt);
-    color.update(dt);
-    position.update(dt);
-    rotation.update(dt);
-    T::update();
-}
-
 template <>
-void ofxAnimatableObject<ofImage>::update(float dt){
-    size.update(dt);
-    color.update(dt);
-    position.update(dt);
-    rotation.update(dt);
-}
+class ofxAnimatableObject<ofImage> : public ofImage {
+public:
+    ofxAnimatableObject() : ofImage(){
+        size.reset(1.);
+        color.setColor(ofColor(255));
+        position.setPosition(ofPoint(0.,0.,0.));
+        rotation.setPosition(ofPoint(0.,0.,0.));
+    }
+    
+    ~ofxAnimatableObject(){};
+    
+    void update(float dt){
+        size.update(dt);
+        color.update(dt);
+        position.update(dt);
+        rotation.update(dt);
+    }
+    
+    void draw(){
+        ofPushMatrix();
+        ofPushStyle();
+        ofTranslate(position.getCurrentPosition());
+        ofRotateX(rotation.getCurrentPosition().x);
+        ofRotateY(rotation.getCurrentPosition().y);
+        ofRotateZ(rotation.getCurrentPosition().z);
+        if(size.val()<0) ofScale(0.,0.,0.);
+        else ofScale(size.val(),size.val(),size.val());
+        ofSetColor(color.getCurrentColor());
+        ofImage::draw(0,0);
+        ofPopStyle();
+        ofPopMatrix();
+    }
+    
+    void setPosition(ofPoint p){position.setPosition(p);}
+    void setPosition(float x, float y,float z=0.){setPosition(ofPoint(x,y,z));}
+    
+    void setSize(float s){size.reset(s);}
+    
+    void setColor(ofColor c){color.setColor(c);}
+    void setColor(int r, int g, int b, int a=255){setColor(ofColor(r,g,b,a));}
+    void setColor(int b, int a){setColor(ofColor(b,a));}
+    void setColor(int b){setColor(ofColor(b));}
+    
+    void setRotation(ofPoint r){rotation.setPosition(r);}
+    void setRotation(float x, float y=0., float z=0.){setRotation(ofPoint(x,y,z));}
+    
+    bool isOrWillBeAnimating(){
+        return (position.isOrWillBeAnimating() || size.isOrWillBeAnimating() || color.isOrWillBeAnimating() || rotation.isOrWillBeAnimating());
+    }
+    
+    ofxAnimatableFloat      size;
+    ofxAnimatableOfColor	color;
+    ofxAnimatableOfPoint	position;
+    ofxAnimatableOfPoint    rotation;
+    
+};
