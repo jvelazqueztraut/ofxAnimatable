81f149ffe01ceda61fe2eb84a4dcaf06b8a7d0e1
jvelazqueztraut <j.velazqueztraut@gmail.com>
2014-02-20 13:35:10 -0300

    Removed the updates from the animatableObject, mainly. Not really convinced.

--- src/ofxAnimatableObject.h
+++ src/ofxAnimatableObject.h
@@ -12,6 +12,17 @@
 #include "ofxAnimatableOfPoint.h"
 #include "ofxAnimatableOfColor.h"
 
+/*template <class T, void (T::*)() > struct HasUpdate;
+
+template <class T>
+void doUpdate( HasUpdate<T,T::update>* ) { T::update(); }
+
+template <class T>
+void doUpdate( HasUpdate<T,T::update>* , T* ptr) { ptr->update(); }
+
+template <class T>
+void doUpdate( ... ) {}*/
+
 template <class T>
 class ofxAnimatableObject : public T {
 public:
@@ -30,7 +41,7 @@ public:
         color.update(dt);
         position.update(dt);
         rotation.update(dt);
-        T::update();
+        //doUpdate<T>(0);
     }
 
     void draw(){
@@ -82,7 +93,6 @@ public:
     ofxAnimatableOfColor	color;
     ofxAnimatableOfPoint	position;
     ofxAnimatableOfPoint    rotation;
-
 };
 
 template <class T>
@@ -107,7 +117,7 @@ public:
         color.update(dt);
         position.update(dt);
         rotation.update(dt);
-        _ptr->update();
+        //doUpdate<T,_ptr>(0);
     }
     
     void draw(){
@@ -154,232 +164,11 @@ public:
 		return rect.inside(p);
 	}
 
+
     T *                     _ptr;
 	ofVec2f					anchor;
     ofxAnimatableFloat      size;
     ofxAnimatableOfColor	color;
     ofxAnimatableOfPoint	position;
     ofxAnimatableOfPoint    rotation;
-    
-};
-
-template <>
-class ofxAnimatableObject<ofImage> : public ofImage {
-public:
-    ofxAnimatableObject() : ofImage(){
-        size.reset(1.);
-        color.setColor(ofColor(255));
-        position.setPosition(ofPoint(0.,0.,0.));
-        rotation.setPosition(ofPoint(0.,0.,0.));
-		anchor.set(0,0);
-    }
-    
-    ~ofxAnimatableObject(){};
-    
-    void update(float dt){
-        size.update(dt);
-        color.update(dt);
-        position.update(dt);
-        rotation.update(dt);
-    }
-    
-    void draw(){
-        ofPushMatrix();
-        ofPushStyle();
-        ofTranslate(position.getCurrentPosition());
-        ofRotateX(rotation.getCurrentPosition().x);
-        ofRotateY(rotation.getCurrentPosition().y);
-        ofRotateZ(rotation.getCurrentPosition().z);
-        if(size.val()<0) ofScale(0.,0.,0.);
-        else ofScale(size.val(),size.val(),size.val());
-        ofSetColor(color.getCurrentColor());
-        ofImage::draw(0,0);
-        ofPopStyle();
-        ofPopMatrix();
-    }
-    
-    void setPosition(ofPoint p){position.setPosition(p);}
-    void setPosition(float x, float y,float z=0.){setPosition(ofPoint(x,y,z));}
-    
-    void setSize(float s){size.reset(s);}
-    
-    void setColor(ofColor c){color.setColor(c);}
-    void setColor(int r, int g, int b, int a=255){setColor(ofColor(r,g,b,a));}
-    void setColor(int b, int a){setColor(ofColor(b,a));}
-    void setColor(int b){setColor(ofColor(b));}
-    
-    void setRotation(ofPoint r){rotation.setPosition(r);}
-    void setRotation(float x, float y=0., float z=0.){setRotation(ofPoint(x,y,z));}
-    
-    bool isOrWillBeAnimating(){
-        return (position.isOrWillBeAnimating() || size.isOrWillBeAnimating() || color.isOrWillBeAnimating() || rotation.isOrWillBeAnimating());
-    }
-
-	void setAnchorPercent(float xPct,float yPct){
-		ofImage::setAnchorPercent(xPct,yPct);
-		anchor.set(xPct,yPct);
-	}
-
-	bool inside(ofPoint p){
-		int width=ofImage::getWidth();
-		int height=ofImage::getHeight();
-		ofRectangle rect(position.getCurrentPosition()-anchor*ofPoint(width,height),width,height);
-		return rect.inside(p);
-	}
-    
-	ofVec2f					anchor;
-    ofxAnimatableFloat      size;
-    ofxAnimatableOfColor	color;
-    ofxAnimatableOfPoint	position;
-    ofxAnimatableOfPoint    rotation;
-    
-};
-
-template <>
-class ofxAnimatableObject<ofFbo> : public ofFbo {
-public:
-    ofxAnimatableObject() : ofFbo(){
-        size.reset(1.);
-        color.setColor(ofColor(255));
-        position.setPosition(ofPoint(0.,0.,0.));
-        rotation.setPosition(ofPoint(0.,0.,0.));
-		anchor.set(0,0);
-    }
-    
-    ~ofxAnimatableObject(){};
-    
-    void update(float dt){
-        size.update(dt);
-        color.update(dt);
-        position.update(dt);
-        rotation.update(dt);
-    }
-    
-    void draw(){
-        ofPushMatrix();
-        ofPushStyle();
-        ofTranslate(position.getCurrentPosition());
-        ofRotateX(rotation.getCurrentPosition().x);
-        ofRotateY(rotation.getCurrentPosition().y);
-        ofRotateZ(rotation.getCurrentPosition().z);
-        if(size.val()<0) ofScale(0.,0.,0.);
-        else ofScale(size.val(),size.val(),size.val());
-        ofSetColor(color.getCurrentColor());
-        ofFbo::draw(0,0);
-        ofPopStyle();
-        ofPopMatrix();
-    }
-    
-    void setPosition(ofPoint p){position.setPosition(p);}
-    void setPosition(float x, float y,float z=0.){setPosition(ofPoint(x,y,z));}
-    
-    void setSize(float s){size.reset(s);}
-    
-    void setColor(ofColor c){color.setColor(c);}
-    void setColor(int r, int g, int b, int a=255){setColor(ofColor(r,g,b,a));}
-    void setColor(int b, int a){setColor(ofColor(b,a));}
-    void setColor(int b){setColor(ofColor(b));}
-    
-    void setRotation(ofPoint r){rotation.setPosition(r);}
-    void setRotation(float x, float y=0., float z=0.){setRotation(ofPoint(x,y,z));}
-    
-    bool isOrWillBeAnimating(){
-        return (position.isOrWillBeAnimating() || size.isOrWillBeAnimating() || color.isOrWillBeAnimating() || rotation.isOrWillBeAnimating());
-    }
-    
-	void setAnchorPercent(float xPct,float yPct){
-		ofFbo::setAnchorPercent(xPct,yPct);
-		anchor.set(xPct,yPct);
-	}
-    
-	bool inside(ofPoint p){
-		int width=ofFbo::getWidth();
-		int height=ofFbo::getHeight();
-		ofRectangle rect(position.getCurrentPosition()-anchor*ofPoint(width,height),width,height);
-		return rect.inside(p);
-	}
-    
-	ofVec2f					anchor;
-    ofxAnimatableFloat      size;
-    ofxAnimatableOfColor	color;
-    ofxAnimatableOfPoint	position;
-    ofxAnimatableOfPoint    rotation;
-    
-};
-
-template <>
-class ofxAnimatableObject<ofImage *> {
-public:
-    ofxAnimatableObject(){
-        size.reset(1.);
-        color.setColor(ofColor(255));
-        position.setPosition(ofPoint(0.,0.,0.));
-        rotation.setPosition(ofPoint(0.,0.,0.));
-		anchor.set(0,0);
-    }
-    
-    ~ofxAnimatableObject(){};
-    
-    void setImage(ofImage * t){
-        _ptr=t;
-    }
-    
-    void update(float dt){
-        size.update(dt);
-        color.update(dt);
-        position.update(dt);
-        rotation.update(dt);
-    }
-    
-    void draw(){
-        ofPushMatrix();
-        ofPushStyle();
-        ofTranslate(position.getCurrentPosition());
-        ofRotateX(rotation.getCurrentPosition().x);
-        ofRotateY(rotation.getCurrentPosition().y);
-        ofRotateZ(rotation.getCurrentPosition().z);
-        if(size.val()<0) ofScale(0.,0.,0.);
-        else ofScale(size.val(),size.val(),size.val());
-        ofSetColor(color.getCurrentColor());
-        _ptr->draw(0,0);
-        ofPopStyle();
-        ofPopMatrix();
-    }
-    
-    void setPosition(ofPoint p){position.setPosition(p);}
-    void setPosition(float x, float y,float z=0.){setPosition(ofPoint(x,y,z));}
-    
-    void setSize(float s){size.reset(s);}
-    
-    void setColor(ofColor c){color.setColor(c);}
-    void setColor(int r, int g, int b, int a=255){setColor(ofColor(r,g,b,a));}
-    void setColor(int b, int a){setColor(ofColor(b,a));}
-    void setColor(int b){setColor(ofColor(b));}
-    
-    void setRotation(ofPoint r){rotation.setPosition(r);}
-    void setRotation(float x, float y=0., float z=0.){setRotation(ofPoint(x,y,z));}
-    
-    bool isOrWillBeAnimating(){
-        return (position.isOrWillBeAnimating() || size.isOrWillBeAnimating() || color.isOrWillBeAnimating() || rotation.isOrWillBeAnimating());
-    }
-
-	void setAnchorPercent(float xPct,float yPct){
-		_ptr->setAnchorPercent(xPct,yPct);
-		anchor.set(xPct,yPct);
-	}
-
-	bool inside(ofPoint p){
-		int width=_ptr->getWidth();
-		int height=_ptr->getHeight();
-		ofRectangle rect(position.getCurrentPosition()-anchor*ofPoint(width,height),width,height);
-		return rect.inside(p);
-	}
-
-    ofImage *               _ptr;
-	ofVec2f					anchor;
-    ofxAnimatableFloat      size;
-    ofxAnimatableOfColor	color;
-    ofxAnimatableOfPoint	position;
-    ofxAnimatableOfPoint    rotation;
-    
 };
---
+++ src/ofxAnimatableObjectMasked.h
@@ -0,0 +1,220 @@
+//
+//  ofxImageMasked.h
+//  unmaskImage
+//
+//  Created by Zaira on 05/02/14.
+//
+//
+
+#pragma once
+#include "ofMain.h"
+#include "ofxAnimatableObject.h"
+#include "ofxAnimatableFloat.h"
+
+enum MaskType{
+    RECTANGULAR=0,
+    CIRCULAR,
+    DIAGONAL,
+    ARROW,
+    MULTI_ARROW,
+    RECTANGLES,
+    HORIZONTAL,
+    VERTICAL
+};
+
+#define RECTANGLES_X 5
+#define RECTANGLES_Y 3
+#define RECTANGLES_DELAY 0.05
+#define RECTANGLES_TIME 0.2
+template <class T>
+class ofxAnimatableObjectMasked : public ofxAnimatableObject<ofFbo>{
+public:
+    ofxAnimatableObjectMasked() : ofxAnimatableObject<ofFbo>(){
+        string shaderProgram =
+		"#extension GL_ARB_texture_rectangle: enable\n\
+		\n\
+		uniform sampler2DRect tex;\n\
+		\n\
+		void main (void){\n\
+        vec2 pos = gl_FragCoord.xy;\n\
+		vec4 color = texture2DRect(tex,pos);\n\
+		gl_FragColor = color;\n\
+		}";
+        
+		shader.setupShaderFromSource(GL_FRAGMENT_SHADER, shaderProgram);
+		shader.linkProgram();
+        
+        mask.reset(1.);
+        
+        type = DIAGONAL;
+    }
+    
+    ~ofxAnimatableObjectMasked(){};
+    
+    void allocate(){
+        width = tex.getWidth();
+        height = tex.getHeight();
+            
+        ofFbo::allocate(width,height,GL_RGBA);
+            
+        radius = 0.5 * sqrt( width*width + height*height );
+    }
+    
+    void update(float dt){
+        
+        if(!ofFbo::isAllocated()){
+            allocate();
+        }
+        
+        ofxAnimatableObject<ofFbo>::update(dt);
+        
+        mask.update(dt);
+        
+        ofPushStyle();
+        ofPushMatrix();
+        ofFill();
+        ofEnableAlphaBlending();
+        glBlendFuncSeparate(GL_ONE, GL_SRC_COLOR, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+        if(mask.val()==1.){
+            ofFbo::begin();
+            ofClear(0,0);
+            ofSetColor(255);
+            tex.draw(0,0);
+            ofFbo::end();
+        }
+        else{
+            ofFbo::begin();
+            ofClear(0,0);
+            ofSetColor(255);
+            if(aux.isAllocated()){
+                aux.draw(0,0);
+            }
+            switch(type){
+                case RECTANGULAR:
+                    shader.begin();
+                    shader.setUniformTexture("tex",tex.getTextureReference(),0);
+                    ofRect(0,0,width*mask.val(),height);
+                    shader.end();
+                    break;
+                case CIRCULAR:
+                    shader.begin();
+                    shader.setUniformTexture("tex",tex.getTextureReference(),0);
+                    ofCircle(width*this->anchor.x,height*this->anchor.y,radius*mask.val());
+                    shader.end();
+                    break;
+                case DIAGONAL:
+                    shader.begin();
+                    shader.setUniformTexture("tex",tex.getTextureReference(),0);
+                    ofSetPolyMode(OF_POLY_WINDING_ODD);	// this is the normal mode
+                    ofBeginShape();
+                    ofVertex(0,0);
+                    ofVertex((width+height)*mask.val(),0);
+                    ofVertex((width+height)*mask.val()-height,height);
+                    ofVertex(0,height);
+                    ofEndShape();
+                    shader.end();
+                    break;
+                case ARROW:
+                    shader.begin();
+                    shader.setUniformTexture("tex",tex.getTextureReference(),0);
+                    ofSetPolyMode(OF_POLY_WINDING_ODD);	// this is the normal mode
+                    ofBeginShape();
+                    ofVertex(0,0);
+                    ofVertex((width+height*0.5)*mask.val()-height*0.5,0);
+                    ofVertex((width+height*0.5)*mask.val(),height*0.5);
+                    ofVertex((width+height*0.5)*mask.val()-height*0.5,height);
+                    ofVertex(0,height);
+                    ofEndShape();
+                    shader.end();
+                    break;
+                case MULTI_ARROW:
+                    shader.begin();
+                    shader.setUniformTexture("tex",tex.getTextureReference(),0);
+                    ofSetPolyMode(OF_POLY_WINDING_ODD);	// this is the normal mode
+                    ofBeginShape();
+                    ofVertex(0,0);
+                    ofVertex((width+height*1.75)*mask.val()-height*1.75,0);
+                    ofVertex((width+height*1.75)*mask.val()-height*1.25,height*0.5);
+                    ofVertex((width+height*1.75)*mask.val()-height*1.75,height);
+                    ofVertex(0,height);
+                    ofEndShape();
+                    ofBeginShape();
+                    ofVertex((width+height*1.75)*mask.val()-height,0);
+                    ofVertex((width+height*1.75)*mask.val()-height*0.5,0);
+                    ofVertex((width+height*1.75)*mask.val(),height*0.5);
+                    ofVertex((width+height*1.75)*mask.val()-height*0.5,height);
+                    ofVertex((width+height*1.75)*mask.val()-height,height);
+                    ofVertex((width+height*1.75)*mask.val()-height*0.5,height*0.5);
+                    ofEndShape();
+                    shader.end();
+                    break;
+                case RECTANGLES:
+                    shader.begin();
+                    shader.setUniformTexture("tex",tex.getTextureReference(),0);
+                    ofSetRectMode(OF_RECTMODE_CENTER);
+                    for(int x=0;x<RECTANGLES_X;x++){
+                        for(int y=0;y<RECTANGLES_Y;y++){
+                            if(mask.val()>(x*RECTANGLES_Y+y)*RECTANGLES_DELAY){
+                                if((mask.val()-(x*RECTANGLES_Y+y)*RECTANGLES_DELAY)/RECTANGLES_TIME<1.){
+                                    ofRect((x*2+1)*width/(RECTANGLES_X*2),(y*2+1)*height/(RECTANGLES_Y*2),width/(RECTANGLES_X)*(mask.val()-(x*RECTANGLES_Y+y)*RECTANGLES_DELAY)/RECTANGLES_TIME,height/(RECTANGLES_Y)*(mask.val()-(x*RECTANGLES_Y+y)*RECTANGLES_DELAY)/RECTANGLES_TIME);
+                                }
+                                else{
+                                    ofRect((x*2+1)*width/(RECTANGLES_X*2),(y*2+1)*height/(RECTANGLES_Y*2),width/RECTANGLES_X,height/RECTANGLES_Y);
+                                }
+                            }
+                        }
+                    }
+                    shader.end();
+                    break;
+                case HORIZONTAL:
+                    ofTranslate(width-mask.val()*width,0);
+                    tex.draw(0,0);
+                    break;
+                case VERTICAL:
+                    ofTranslate(0,height-mask.val()*height);
+                    tex.draw(0,0);
+                    break;
+            }
+            
+            ofFbo::end();
+        }
+        ofPopMatrix();
+        ofPopStyle();
+    }
+    
+    /*void draw(){
+        ofPushMatrix();
+        ofPushStyle();
+        ofTranslate(this->position.getCurrentPosition());
+        ofRotateX(this->rotation.getCurrentPosition().x);
+        ofRotateY(this->rotation.getCurrentPosition().y);
+        ofRotateZ(this->rotation.getCurrentPosition().z);
+        if(this->size.val()<0) ofScale(0.,0.,0.);
+        else ofScale(this->size.val(),this->size.val(),this->size.val());
+        ofSetColor(this->color.getCurrentColor());
+        fbo.draw(0,0);
+        ofPopStyle();
+        ofPopMatrix();
+    }*/
+    
+    bool isOrWillBeAnimating(){
+        return (mask.isOrWillBeAnimating() || ofxAnimatableObject<ofFbo>::isOrWillBeAnimating());
+    }
+    
+    void setMask(float m){mask.reset(m);}
+    
+    void setMaskType(MaskType t){
+        type=t;
+    }
+    
+    ofxAnimatableFloat mask;
+    T tex;
+    T aux;
+private:
+    //ofFbo fbo;
+    ofShader shader;
+    MaskType type;
+    float width;
+    float height;
+    float radius;
+};
