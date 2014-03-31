adc75d6222c307c22344aef52f4c6d496d5d7974
masOTROS <javier@masotros.com>
2014-03-30 22:15:55 -0300

    Shaders version.

--- exampleAdvanced/src/testApp.h
+++ exampleAdvanced/src/testApp.h
@@ -21,9 +21,10 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    float time;
     
     ofxAnimatableObject<ofImage> img;
     
-    ofxAnimatableObjectMasked<ofTexture> imgMasked;
+    ofxAnimatableObjectMasked<ofTexture,1> imgMasked;
 		
 };
